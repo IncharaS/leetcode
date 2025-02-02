93d3fae5d9a0edb10d15fcbc64ca41fe8af610e9
IncharaS <60838958+IncharaS@users.noreply.github.com>
2025-02-02 16:39:57 -0500

    [1834] [Time Beats: 94.83%] [Memory Beats: 5.17%] - LeetPush

--- 1834-Single-Threaded-CPU.py
+++ 1834-Single-Threaded-CPU.py
@@ -21,4 +21,19 @@ class Solution(object):
             processing_time, index = heapq.heappop(min_heap)
             output.append(index)
             time += processing_time  
+            ## because time is always the current processing time
         return output
+
+        ''' the system time is 0.
+        Now scan from first, the first task is available at 1, 
+        so you'll update the sytem time to 1.
+        And now add all the process which are available at this time 
+        into the Heap. (processing time, index)
+        Pop ONLY 1 element out of heap (seriel execution)
+        if this task taken x processing time, then the current 
+        system time is time += x,
+        by this time there will be more processes available.
+        so add all of them. and repeat until:
+        1. len(task) is reached = nothing to add
+        2. heap is empty = nothing to pop'''
+        
