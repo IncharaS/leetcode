d5988e12166b6d5de4ba907be7a9fa88322dbbc6
IncharaS <60838958+IncharaS@users.noreply.github.com>
2025-02-03 23:07:19 -0500

    [643] [Time Beats: 51.87%] [Memory Beats: 55.82%] - LeetPush

---
+++ 643-Maximum-Average-Subarray-I.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        \\\
+        :type nums: List[int]
+        :type k: int
+        :rtype: float
+        \\\
+        n = len(nums)
+        sum_window = sum(nums[:k])
+        max_sum = sum_window
+        for i in range(len(nums)-k):
+            sum_window -= nums[i]
+            sum_window += nums[i+k]
+            max_sum = max(max_sum, sum_window)
+        return max_sum/float(k)
+        
