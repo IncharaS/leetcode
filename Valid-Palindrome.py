25594913a2c891037af5781f6ac59d0809f46c48
IncharaS <60838958+IncharaS@users.noreply.github.com>
2025-04-03 19:52:50 -0400

    [125] [Time Beats: 84.83%] [Memory Beats: 80.86%] - LeetPush

--- Valid-Palindrome.py
+++ Valid-Palindrome.py
@@ -1,17 +1,22 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s) - 1
-
-        while l<r:
-            while l<r and not self.alphanum(s[l]):
-                l += 1
-            while l<r and not self.alphanum(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
+class Solution(object):
+    def isPalindrome(self, s):
+        \\\
+        :type s: str
+        :rtype: bool
+        \\\
+        s = s.lower()
+        i, j = 0, len(s) - 1
+        
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+            
+            if s[i] != s[j]:
                 return False
-            l += 1
-            r -= 1
+            
+            i += 1
+            j -= 1
+        
         return True
-
-    def alphanum(self, c):
-        return( ord("A") <= ord(c) <= ord("Z") or ord("a") <= ord(c) <= ord("z") or ord("0") <= ord(c) <= ord("9"))
