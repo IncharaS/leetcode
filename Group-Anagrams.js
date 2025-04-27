c75fc83fb6a0901b4e52737a593a36c871616d10
IncharaS <60838958+IncharaS@users.noreply.github.com>
2025-04-27 17:35:57 -0400

    [49] [Time Beats: 29.83%] [Memory Beats: 62.62%] - LeetPush

---
+++ Group-Anagrams.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const map = new Map();
+    for (const word of strs){
+        const sorted = word.split('').sort().join('')
+        if (!map.has(sorted)){
+            map.set(sorted, [])
+        }
+        map.get(sorted).push(word)
+    }
+    return Array.from(map.values())
+};
