558058d89368d322d116efff92710089f3b1d4e7
IncharaS <60838958+IncharaS@users.noreply.github.com>
2025-05-29 00:02:34 -0400

    [572] [Time Beats: 95.20%] [Memory Beats: 38.95%] - LeetPush

--- Subtree-of-Another-Tree.py
+++ Subtree-of-Another-Tree.py
@@ -1,18 +1,31 @@
 class Solution:
     def isSubtree(self, root, subRoot):
-        if not root:
-            return False
+    #     if not root:
+    #         return False
 
-        if self.same(root, subRoot):
-            return True
+    #     if self.same(root, subRoot):
+    #         return True
 
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+    #     return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
 
-    def same(self, a, b):
-        if not a and not b:
-            return True
-        if not a or not b:
-            return False
-        if a.val != b.val:
-            return False
-        return self.same(a.left, b.left) and self.same(a.right, b.right)
+    # def same(self, a, b):
+    #     if not a and not b:
+    #         return True
+    #     if not a or not b:
+    #         return False
+    #     if a.val != b.val:
+    #         return False
+    #     return self.same(a.left, b.left) and self.same(a.right, b.right)
+        def helper(root, res):
+            if root:
+                helper(root.left, res)
+                helper(root.right, res)
+                res.append(f\#{root.val},\)
+            else:
+                res.append(f\##\)
+
+        res = []
+        helper(root, res)
+        res1 = []
+        helper(subRoot, res1)
+        return \\.join(res1) in \\.join(res)
