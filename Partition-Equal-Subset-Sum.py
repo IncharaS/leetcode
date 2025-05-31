b6bfb83af46a6e454887789a2b465f0d2e24362c
IncharaS <60838958+IncharaS@users.noreply.github.com>
2025-05-30 23:22:07 -0400

    [416] [Time Beats: 44.14%] [Memory Beats: 96.47%] - LeetPush

--- Partition-Equal-Subset-Sum.py
+++ Partition-Equal-Subset-Sum.py
@@ -1,3 +1,26 @@
+# class Solution:
+#     def canPartition(self, nums):
+#         total = sum(nums)
+#         if total % 2 != 0:
+#             return False
+
+#         target = total // 2
+#         n = len(nums)
+
+#         dp = [[False] * (target + 1) for _ in range(n + 1)]
+        
+#         # Base case: sum 0 is always possible (empty subset)
+#         for i in range(n + 1):
+#             dp[i][0] = True
+
+#         for i in range(1, n + 1):
+#             for j in range(1, target + 1):
+#                 if j < nums[i - 1]:
+#                     dp[i][j] = dp[i - 1][j]  # can't include the number
+#                 else:
+#                     dp[i][j] = dp[i - 1][j] or dp[i - 1][j - nums[i - 1]]
+
+#         return dp[n][target]
 class Solution:
     def canPartition(self, nums):
         total = sum(nums)
@@ -5,19 +28,11 @@ class Solution:
             return False
 
         target = total // 2
-        n = len(nums)
+        dp = [False] * (target + 1)
+        dp[0] = True  # sum 0 is always possible
 
-        dp = [[False] * (target + 1) for _ in range(n + 1)]
-        
-        # Base case: sum 0 is always possible (empty subset)
-        for i in range(n + 1):
-            dp[i][0] = True
+        for num in nums:
+            for j in range(target, num - 1, -1):
+                dp[j] = dp[j] or dp[j - num]
 
-        for i in range(1, n + 1):
-            for j in range(1, target + 1):
-                if j < nums[i - 1]:
-                    dp[i][j] = dp[i - 1][j]  # can't include the number
-                else:
-                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - nums[i - 1]]
-
-        return dp[n][target]
+        return dp[target]
