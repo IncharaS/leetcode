9fc45f44e42462eeb68cc370a82e9caf96fd99d0
IncharaS <60838958+IncharaS@users.noreply.github.com>
2025-06-05 20:04:20 -0400

    [146] [Time Beats: 94.84%] [Memory Beats: 76.75%] - LeetPush

---
+++ LRU-Cache.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        ## initialize both dict and dll
+        self.cache = {}
+        self.capacity = capacity
+        self.oldest = Node(0, 0)
+        self.latest = Node(0, 0)
+        self.oldest.next = self.latest
+        self.latest.prev = self.oldest
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insertAtHead(self.cache[key])
+            return self.cache[key].value
+        return -1
+
+    def remove(self, node):
+        prev, next = node.prev, node.next
+        prev.next = next
+        next.prev = prev
+    
+    def insertAtHead(self, node):
+        prev, next = self.latest.prev, self.latest
+        prev.next = next.prev = node
+        node.next = next
+        node.prev = prev
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insertAtHead(self.cache[key])
+
+        if len(self.cache) > self.capacity:
+            lru = self.oldest.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
