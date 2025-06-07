4f318c5a2f1d0eb68384356de1d7b7b0c8cf9471
IncharaS <60838958+IncharaS@users.noreply.github.com>
2025-06-07 16:37:48 -0400

    [695] [Time Beats: 35.24%] [Memory Beats: 93.10%] - LeetPush

--- Max-Area-of-Island.py
+++ Max-Area-of-Island.py
@@ -1,16 +1,20 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         def dfs(i, j):
-            nonlocal c
-            visited.add((i,j))
+            c = 1
             directions = [(-1,0), (0, -1), (1, 0), (0, 1)]
-            for a,b in directions:
-                ni = i+a
-                nj = j+b
-                if 0 <= ni < len(grid) and 0 <= nj < len(grid[0]):
-                    if grid[ni][nj] == 1 and (ni, nj) not in visited:
-                        c += 1
-                        dfs(ni, nj)
+            stack = [(i,j)]
+            visited.add((i,j))
+            while stack:
+                i,j = stack.pop() #LIFO
+                for a,b in directions:
+                    ni = i+a
+                    nj = j+b
+                    if 0 <= ni < len(grid) and 0 <= nj < len(grid[0]):
+                        if grid[ni][nj] == 1 and (ni, nj) not in visited:
+                            c += 1
+                            stack.append((ni,nj))
+                            visited.add((ni,nj))
             return c
             
 
@@ -19,7 +23,6 @@ class Solution:
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if (i,j) not in visited and grid[i][j] == 1:
-                    c = 1
                     count = dfs(i,j)
                     max_count = max(count, max_count)
         return max_count
