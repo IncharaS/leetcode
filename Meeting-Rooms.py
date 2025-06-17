1bce9c63d9367108fdf76ea7a8db2ae2e3e506a1
IncharaS <60838958+IncharaS@users.noreply.github.com>
2025-06-17 14:06:27 -0400

    [252] [Time Beats: 100.00%] [Memory Beats: 35.85%] - LeetPush

--- Meeting-Rooms.py
+++ Meeting-Rooms.py
@@ -1,12 +1,12 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        sorted_intervals = sorted(intervals, key=lambda x: x[1])
-        length = len(sorted_intervals)
+        intervals.sort(key = lambda i: i[1])
+        length = len(intervals)
         if length == 0 or length == 1:
             return True
-        previous_interval_end = sorted_intervals[0][1]
+        previous_interval_end = intervals[0][1]
         for interval in range(1, length):
-            if sorted_intervals[interval][0] < previous_interval_end:
+            if intervals[interval][0] < previous_interval_end:
                 return False
-            previous_interval_end = sorted_intervals[interval][1]
+            previous_interval_end = intervals[interval][1]
         return True
