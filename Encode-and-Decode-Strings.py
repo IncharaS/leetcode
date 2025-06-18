1b6b1ec1a95d07a0cf420eda05e2e2cc65b2f836
IncharaS <60838958+IncharaS@users.noreply.github.com>
2025-06-18 14:47:49 -0400

    [271] [Time Beats: 93.99%] [Memory Beats: 99.07%] - LeetPush

--- Encode-and-Decode-Strings.py
+++ Encode-and-Decode-Strings.py
@@ -2,41 +2,41 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        string = ''
-        for s in strs:
-            for i in s:
-                if i == '/':
-                    string += '/'
-                string += i
-            string += '/:'
-        # print(string)
-
-        return string
+        return '&*%$#@!'.join(strs)
+        # string = ''
+        # for s in strs:
+        #     for i in s:
+        #         if i == '/':
+        #             string += '/'
+        #         string += i
+        #     string += '/:'
+        # return string
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        length = len(s)
-        i = 0
-        res = []
-        string = ''
-        while i < length:
-            if s[i] == '/':
-                # there are 2 options
-                # 1. if / is next, it was just added to encode
-                # 2. if : is next, it was delimiter
-                i += 1
-                if i < length and s[i] == ':':
-                    res.append(string)
-                    string = ''
-                elif i < length and s[i] == '/':
-                    string += '/'
-                i += 1
-            else:
-                string += s[i]
-                i += 1
-            # print(string)
-        return res
+        return s.split('&*%$#@!')
+        # length = len(s)
+        # i = 0
+        # res = []
+        # string = ''
+        # while i < length:
+        #     if s[i] == '/':
+        #         # there are 2 options
+        #         # 1. if / is next, it was just added to encode
+        #         # 2. if : is next, it was delimiter
+        #         i += 1
+        #         if i < length and s[i] == ':':
+        #             res.append(string)
+        #             string = ''
+        #         elif i < length and s[i] == '/':
+        #             string += '/'
+        #         i += 1
+        #     else:
+        #         string += s[i]
+        #         i += 1
+        #     # print(string)
+        # return res
 
 
 
