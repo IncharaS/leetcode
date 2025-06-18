705702fe22f25e71772bb8c03c8ee3f4ea488651
IncharaS <60838958+IncharaS@users.noreply.github.com>
2025-06-18 16:13:23 -0400

    [286] [Time Beats: 85.02%] [Memory Beats: 85.84%] - LeetPush

--- Walls-and-Gates.py
+++ Walls-and-Gates.py
@@ -1,33 +1,27 @@
+from collections import deque
+from typing import List
+
 class Solution:
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
-        """
-        Do not return anything, modify rooms in-place instead.
-        """
-        
-        def bfs(i, j):
-            # if (i,j) in visited:
-            #     return 
-            # visited.add((i, j))
-            directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-            queue = deque([(i, j)])
-            step = 1
-            while queue:
-                length = len(queue)
-                for _ in range(length):
-                    (i_, j_) = queue.popleft()
-                    for [a, b] in directions: 
-                        new_i = i_ + a
-                        new_j = j_ + b
-                        if 0 <= new_i < len(rooms) and 0 <= new_j < len(rooms[0]):
-                            # visited.add((new_i, new_j))
-                            if rooms[new_i][new_j] != -1 and rooms[new_i][new_j] != 0 and rooms[new_i][new_j] > step:
-                                rooms[new_i][new_j] = step
-                                queue.append((new_i, new_j))
-                step += 1
+        if not rooms or not rooms[0]:
+            return
 
-        # visited = set()
-        for i in range(len(rooms)):
-            for j in range(len(rooms[0])):
+        INF = 2**31 - 1
+        rows, cols = len(rooms), len(rooms[0])
+        queue = deque()
+
+        # Step 1: Enqueue all gates (value == 0)
+        for i in range(rows):
+            for j in range(cols):
                 if rooms[i][j] == 0:
-                    bfs(i, j)
-        return rooms
+                    queue.append((i, j))
+
+        # Step 2: BFS from all gates at once
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        while queue:
+            i, j = queue.popleft()
+            for di, dj in directions:
+                ni, nj = i + di, j + dj
+                if 0 <= ni < rows and 0 <= nj < cols and rooms[ni][nj] == INF:
+                    rooms[ni][nj] = rooms[i][j] + 1
+                    queue.append((ni, nj))
